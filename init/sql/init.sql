c4c2ab8916c80219e07097894b43711577820e15
576479566 <576479566@qq.com>
2018-08-05 13:56:19 +0800

    初始化建表sql

---
+++ init/sql/init.sql
@@ -0,0 +1,68 @@
+create table t_category
+(
+	id bigint auto_increment
+		primary key,
+	name varchar(255) not null,
+	type varchar(255) not null,
+	constraint t_category_id_uindex
+		unique (id)
+)
+;
+
+create table t_formula
+(
+	id bigint auto_increment
+		primary key,
+	org_id bigint not null,
+	sub_catagory_id bigint not null,
+	formula varchar(255) not null,
+	constraint t_formula_id_uindex
+		unique (id)
+)
+;
+
+create table t_org
+(
+	id bigint auto_increment
+		primary key,
+	name varchar(255) not null,
+	`limit` bigint not null,
+	term bigint not null,
+	interest_rate varchar(255) null,
+	requirements varchar(255) null,
+	material varchar(255) null,
+	logo varchar(255) null,
+	`desc` varchar(255) null,
+	contacts varchar(255) null,
+	phone varchar(255) null,
+	strengths varchar(255) null,
+	constraint t_org_id_uindex
+		unique (id)
+)
+;
+
+create table t_statistics
+(
+	id bigint auto_increment
+		primary key,
+	org_id bigint not null,
+	count_time timestamp not null,
+	constraint t_statistics_id_uindex
+		unique (id)
+)
+;
+
+create table t_sub_catagory
+(
+	id bigint auto_increment
+		primary key,
+	name varchar(255) not null,
+	category_id bigint not null,
+	`key` varchar(255) not null,
+	constraint t_sub_catagory_id_uindex
+		unique (id),
+	constraint t_sub_catagory_key_uindex
+		unique (key)
+)
+;
+
