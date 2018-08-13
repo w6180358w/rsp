5992dcfd56e18f78c3ec33ddea5c0d9970f22f9c
w6180358w <272416634@163.com>
2018-08-13 23:45:32 +0800

    打war包  手机页面url

--- src/main/java/com/rsp/rsp/RspApplication.java
+++ src/main/java/com/rsp/rsp/RspApplication.java
@@ -2,10 +2,17 @@ package com.rsp.rsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RspApplication {
+public class RspApplication extends SpringBootServletInitializer{
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(RspApplication.class);
+	}
+    
 	public static void main(String[] args) {
 		SpringApplication.run(RspApplication.class, args);
 	}
