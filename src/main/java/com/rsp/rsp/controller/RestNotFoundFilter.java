c1b7156ee8c7eff1c30c940edfc89b34841478b8
576479566 <576479566@qq.com>
2018-08-14 22:00:25 +0800

    后台异常拦截暂时不用,用默认的

--- src/main/java/com/rsp/rsp/controller/RestNotFoundFilter.java
+++ src/main/java/com/rsp/rsp/controller/RestNotFoundFilter.java
@@ -6,20 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 自定义的error页面
  * @author sjb
  */
-@Controller
+//@Controller
 public class RestNotFoundFilter implements ErrorController {
 
     private static final String ERROR_PATH = "/error";
 
     @RequestMapping(value = ERROR_PATH)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public String handleError() {
+    public String handleError(HttpServletRequest request) {
         //这里是自定义的返回内容
-        return ERROR_PATH;
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if(statusCode == 401){
+            return "/401";
+        }else if(statusCode == 404){
+            return ERROR_PATH;
+        }
+        return "";
     }
 
     @Override
