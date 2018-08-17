2cbf0b6245ce62b086c422e6cd9f6883eab23977
sunjibo <576479566@qq.com>
2018-08-17 18:28:33 +0800

    测试查看连接池

--- src/main/java/com/rsp/rsp/RspApplication.java
+++ src/main/java/com/rsp/rsp/RspApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class RspApplication extends SpringBootServletInitializer{
@@ -15,7 +20,15 @@ public class RspApplication extends SpringBootServletInitializer{
     
 	public static void main(String[] args) {
 		try {
-			SpringApplication.run(RspApplication.class, args);
+			ConfigurableApplicationContext context = SpringApplication.run(RspApplication.class, args);
+//			DataSource ds = context.getBean(DataSource.class);
+			//默认的使用的是tomcat的数据源
+//			System.out.println(ds.getClass().getName());
+//			Connection connection = ds.getConnection();
+			//test
+//			System.out.println(connection.getCatalog());
+//			System.out.println(context.getBean(JdbcTemplate.class));
+//			connection.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
